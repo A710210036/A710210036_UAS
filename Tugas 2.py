90190e9ef6c4c5f4148babfd59fa0d6d6e1af502
Karel Aji Pratama <karelpratama50@gmail.com>
2024-06-11 18:10:36 +0700

    Delete Tugas 2.py

--- Tugas 2.py
+++
@@ -1,7 +0,0 @@
-def penjumlahan(a, b):
-    return a + b
-
-angka1 = int(input("Masukkan angka pertama: "))
-angka2 = int(input("Masukkan angka kedua: "))
-hasil = penjumlahan(angka1, angka2)
-print(f"Hasil penjumlahan: {hasil}")
