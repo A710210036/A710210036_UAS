7fddab31b232340cd9896ebe70d1642525b40901
Karel Aji Pratama <karelpratama50@gmail.com>
2024-06-19 00:40:31 +0700

    Create Tugas 6.py
    faktorial

---
+++ Tugas 6.py
@@ -0,0 +1,8 @@
+def faktorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * faktorial(n - 1)
+
+bilangan = int(input("Masukkan sebuah bilangan: "))
+print("Faktorial dari", bilangan, "adalah", faktorial(bilangan))
