172185edf38404bc498bb7508d9064ef7aa8867a
Karel Aji Pratama <karelpratama50@gmail.com>
2024-06-18 15:09:41 +0700

    Create Tugas 5.py
    menampilkan bebrapa karakter menjadi 1

---
+++ Tugas 5.py
@@ -0,0 +1,8 @@
+data_mahasiswa = {
+    "nama": "karel",
+    "umur": 21,
+    "jurusan": "Pendidikan Teknik Informatika"
+}
+
+for kunci, nilai in data_mahasiswa.items():
+    print(kunci + ": " + str(nilai))
