bb8229c2c170c89d116e4d62d66bae11635f6037
Karel Aji Pratama <karelpratama50@gmail.com>
2024-07-06 00:08:40 +0700

    Create Tugas 23.py
    Program dengan Inheritance (Pewarisan)

---
+++ Tugas 23.py
@@ -0,0 +1,13 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        raise NotImplementedError("Subclass must implement abstract method")
+
+class Dog(Animal):
+    def speak(self):
+        return f"{self.name} says Woof!"
+
+dog1 = Dog("Buddy")
+print(dog1.speak())
