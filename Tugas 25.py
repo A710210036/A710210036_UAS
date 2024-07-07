2c3b0ecb7b218ac56cdd3365c7025778feb3a808
Karel Aji Pratama <karelpratama50@gmail.com>
2024-07-08 00:13:03 +0700

    Create Tugas 25.py
    Program dengan Encapsulation

---
+++ Tugas 25.py
@@ -0,0 +1,21 @@
+class BankAccount:
+    def __init__(self, account_number, balance):
+        self.__account_number = account_number
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+        else:
+            print("Insufficient funds")
+
+    def get_balance(self):
+        return self.__balance
+
+account = BankAccount("12345", 1000)
+account.deposit(500)
+account.withdraw(200)
+print(account.get_balance())
