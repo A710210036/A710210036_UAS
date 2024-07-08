f03653fa6fe3dab925982edc9399687a3b0b3aee
Karel Aji Pratama <karelpratama50@gmail.com>
2024-07-09 00:05:47 +0700

    Create Tugas 26.py
    Program dengan Static Method

---
+++ Tugas 26.py
@@ -0,0 +1,11 @@
+class MathOperations:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+print(MathOperations.add(5, 3))
+print(MathOperations.subtract(10, 4))
