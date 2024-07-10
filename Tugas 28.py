1ecff8108083fdc805e5a8ffacb9f417e323542d
Karel Aji Pratama <karelpratama50@gmail.com>
2024-07-11 00:20:10 +0700

    Create Tugas 28.py
    Program dengan Properti

---
+++ Tugas 28.py
@@ -0,0 +1,18 @@
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        if value < 0:
+            raise ValueError("Radius cannot be negative")
+        self._radius = value
+
+circle = Circle(5)
+print(circle.radius)
+circle.radius = 10
+print(circle.radius)
