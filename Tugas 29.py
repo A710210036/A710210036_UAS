49832206d1dd3cf3d13c51114d53da5cc526f050
Karel Aji Pratama <karelpratama50@gmail.com>
2024-07-12 00:14:35 +0700

    Create Tugas 29.py
    Program dengan Abstract Class

---
+++ Tugas 29.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+rectangle = Rectangle(5, 10)
+print(rectangle.area())
